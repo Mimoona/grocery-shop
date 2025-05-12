cb3f50c43677836f32d561aa4c8e6499b1b7f39d
Mimoona <memoona.raheel8@gmail.com>
2025-05-12 11:25:18 +0200

    Add controller

---
+++ backend/src/main/java/com/grocery/shop/backend/controller/groceryController.java
@@ -0,0 +1,9 @@
+package com.grocery.shop.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/grocery-shop")
+public class groceryController {
+}
