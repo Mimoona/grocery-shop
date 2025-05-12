55b8ea67938bfe0c128b91fc6ccaab8c450b4a78
Mimoona <memoona.raheel8@gmail.com>
2025-05-12 11:57:23 +0200

    add get method

--- backend/src/main/java/com/grocery/shop/backend/controller/groceryController.java
+++ backend/src/main/java/com/grocery/shop/backend/controller/groceryController.java
@@ -1,9 +1,15 @@
 package com.grocery.shop.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/grocery-shop")
 public class groceryController {
+
+    @GetMapping("/greet")
+    public String getGreet(){
+        return "hello";
+    }
 }
